2f61edb8e344f2baa3e9317ac203f9924bd0dd71
Maksim Teterin <maksim.padriku@gmail.com>
2026-04-11 12:50:42 +0300

    Add restaurant ownership check in BookingSecurity

--- src/main/java/com/example/restaurantassistantrestapi/Config/BookingSecurity.java
+++ src/main/java/com/example/restaurantassistantrestapi/Config/BookingSecurity.java
@@ -2,6 +2,7 @@ package com.example.restaurantassistantrestapi.Config;
 
 import com.example.restaurantassistantrestapi.model.User;
 import com.example.restaurantassistantrestapi.service.BookingService;
+import com.example.restaurantassistantrestapi.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +10,16 @@ import org.springframework.stereotype.Component;
 public class BookingSecurity {
     @Autowired
     private BookingService bookingService;
+    @Autowired
+    private RestaurantService restaurantService;
 
-    public boolean isOwner(int bookingId, User user) {
+    public boolean isBookingOwner(int bookingId, User user) {
         return bookingService.getBookingById(bookingId)
                 .map(b -> b.getUserId() == (user.getId()))
                 .orElse(false);
+    }
+
+    public boolean isRestaurantOwner(int restaurantId, User user) {
+        return restaurantService.getRestaurantById(restaurantId).orElseThrow().getOwnerId() == user.getId();
     }
 }
